68c06f9080237139d88ea3c9cb0d6aca9fcb92f5
Hasti Narimanzadeh <hasti.nz@gmail.com>
2020-02-14 01:32:33 +0200

    adds optimizer schedule

--- trainer.py
+++ trainer.py
@@ -5,10 +5,14 @@ import numpy as np
 import models
 
 if __name__ == "__main__":
-    batch_size = 200
+    batch_size = 2000
     while True:
         model = models.NeuralNetworkModel()
-        optimizer = tf.keras.optimizers.Adam()
+        lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+                initial_learning_rate=0.001,
+                decay_steps=10000,
+                decay_rate=0.96)
+        optimizer = tf.keras.optimizers.Adam(lr_schedule)
 
         checkpoint_directory = "./training_checkpoints"
         checkpoint_prefix = os.path.join(checkpoint_directory, "ckpt")
@@ -30,7 +34,7 @@ if __name__ == "__main__":
 
             states, policies, values = zip(*history)
             with tf.GradientTape() as tape:
-                 estimated_policies, estimated_values = model(np.array(states))
+                 estimated_policies, estimated_values = model(np.array(states), training=True)
                  losses = model.loss(estimated_policies, estimated_values,
                                     np.array(policies), np.array(values))
             print("loss:", np.mean(losses), "batch size:", len(history))
@@ -38,6 +42,9 @@ if __name__ == "__main__":
             optimizer.apply_gradients(zip(grads, model.trainable_variables))
 
             checkpoint.save(file_prefix=checkpoint_prefix)
+            for f in files:
+                os.remove(f)
+
 
 
         time.sleep(30)
